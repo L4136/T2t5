092690474a4cba31a697fdffd9b350f590b25874
Heli Vepsäläinen <L4136@student.jamk.fi>
2018-02-18 20:30:12 +0200

    T2t5

--- Program.cs
+++ Program.cs
@@ -9,6 +9,46 @@ namespace T2t5
     {
         static void Main(string[] args)
         {
+            int trunkHeight = 2;
+            
+            Console.Write("Give a number: ");
+            int height = System.Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < height; i++)
+            {
+                int j;
+                if (i < (height - trunkHeight))
+                {
+                    j = i;
+                }
+                else
+                {
+                    j = 0;
+                }
+                printSpace(height, j, trunkHeight);
+                
+                printStar(j);
+                Console.WriteLine();
+            }
+        }
+
+        static void printSpace(int height, int i, int trunkHeight)
+        {
+            int whitespace = height - i - trunkHeight - 1;
+            for (int j = 0; j < whitespace; j++)
+            {
+                Console.Write(" ");
+            }
+        }
+
+        static void printStar(int i)
+        {
+            int stars = 2 * i + 1;
+            for (int j = 0; j < stars; j++)
+            {
+                Console.Write("*");
+            }
         }
     }
+    
 }
